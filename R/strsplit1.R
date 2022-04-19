5c3db643cd51f24a9f0cddbcc7160fd11fedfe05
Ioannis Mavrogiannis <imavrog3@gmail.com>
2022-04-19 16:03:04 +0200

    Initial commit

---
+++ R/strsplit1.R
@@ -0,0 +1,16 @@
+
+
+#' Split a string.
+#'
+#' @param x
+#' @param split
+#'
+#' @return
+#' @export
+#'
+#' @examples
+strsplit1 <- function(x, split) {
+    strsplit(x, split = split)[[1]]
+}
+
+
